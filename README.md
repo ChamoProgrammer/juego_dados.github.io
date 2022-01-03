c7a0b3c39df9574bf8cf0b716e515c49b9409ee3
ChamoProgrammer <70454178+ChamoProgrammer@users.noreply.github.com>
2022-01-02 22:48:50 -0600

    commit initial

---
+++ README.md
@@ -0,0 +1,33 @@
+
+# TIRAR LOS DADOS
+- Un juego simple de código abierto que utiliza HTML, CSS y Javascript.
+- Para jugar, haga clic en [ROLL-THE-DICE] (https://fidalmathew.github.io/ROLL-THE-DICE/)
+
+! [Captura de pantalla] (https://github.com/FidalMathew/ROLL-THE-DICE/blob/main/README%20FILES/screenshot.PNG)
+
+## Descripción del juego
+- En este juego tienes que adivinar un número en el dado 🎲 mientras se lanza.
+- Si su suposición es correcta, su puntuación se actualizará en uno.
+- Hay una espera / cuenta regresiva de 10 segundos entre rollos.
+- Una oportunidad de 10 segundos para adivinar el rollo.
+- Cuando hace clic en su conjetura, la tirada ocurre inmediatamente y se informa el resultado.
+- Si los 10 segundos de la tirada caducan, simplemente se reinicia y no hay impacto en su puntuación.
+- ** Ajuste del tamaño de la pantalla: ** Si está en un navegador de escritorio, asegúrese de configurar su navegador en 'Modo de pantalla completa' (** F11 **)
+
+## Contribuciones son bienvenidas
+### Pautas de contribución para este proyecto
+1. Lea los problemas primero. Algunos de ellos son simples y otros son desafiantes.
+2. Intente arreglarlos y luego envíe una solicitud de extracción (PR). Incluir una captura de pantalla
+junto con su PR (lea a continuación).
+3. Cree un nuevo problema si aún no está allí.
+
+#### Cómo agregar una captura de pantalla
+- Por favor, agregue dos archivos PNG de captura de pantalla idénticos en el directorio 'README FILES'.
+    - uno debe llamarse 'screenshot.PNG' y
+    - el otro debe llamarse 'screenshot_xxxxxxxxxx.PNG' donde xxxxxxxxxx es
+    [la marca de tiempo de Unix] (https://www.unixtimestamp.com/)
+    - ** ¿Por qué? ** Las imágenes espaciadas en el tiempo nos ayudan a realizar un seguimiento de los cambios anteriores de la interfaz de usuario
+
+## Recursos
+- Archivos de audio de [www.pixabay.com] (https://pixabay.com/)
+- [Sello de tiempo de Unix] (https://www.unixtimestamp.com/)
---
+++ index.js
@@ -0,0 +1,104 @@
+"use strict";
+let score = 0;
+let randomVar;
+let trialNumber = 1;
+let playMusic = true;
+let buttonVal = document.getElementsByTagName("button");
+let result = document.getElementById("result");
+let report = document.getElementById("text-init-loading");
+let timeS = document.getElementById("timeS");
+let dice = document.getElementById("dice");
+const Clicked = (e) => {
+  let userSelectedNumber = (e.target.innerText);
+  dice.src = `img/${randomVar}.png`;
+  document.getElementById("text-user-selected").innerHTML = userSelectedNumber;
+  document.getElementById("text-user-selected").style.visibility="visible";
+  if (randomVar == userSelectedNumber) {
+    result.style.color = "#1CE1BE";
+    result.innerHTML = "¡Lo adivinaste bien!🎉🥳";
+    score++;
+    document.getElementById("text-total-score").innerHTML = score;
+    //playSoundSuccess();
+    playSound("right");
+  } else {
+    result.style.color = "#FF6D2E";
+    result.innerHTML = "Número incorrecto!🥺";
+    //playSoundFailure();
+    playSound("wrong");
+  }
+  Array.from(buttonVal).forEach(element => {
+    element.disabled = true;
+    element.classList.toggle("disabled");
+  });
+  trialNumber++;
+  document.getElementById("text-ttrial-number").innerHTML = trialNumber;
+}
+const load = () => {
+  report.innerHTML = `<h2>Cargando...🔁</h2>
+    <span style="font-size:0.7em;">
+    El juego comenzará en unos segundos!😉
+    </span>`;
+  document.getElementById('h').style.visibility="hidden";
+  result.style.visibility="hidden";
+  document.getElementById("text-user-selected").style.visibility="hidden";
+  Array.from(buttonVal).forEach(element => {
+    element.addEventListener("click", Clicked);
+    element.disabled = false;
+    element.classList.add("disabled");
+  });
+  setTimeout(() => {
+    report.style.visibility="hidden";
+    document.getElementById('h').style.visibility="visible";
+  }, 6000);
+}
+const DisplayTime = () => {
+  let timesecond = 2;
+  setInterval(() => {
+    timeS.innerHTML = timesecond;
+    timesecond--;
+    if (timesecond == 0) {
+      result.style.color = "#FFFFFF";
+      result.innerHTML="Puedes adivinarlo?🤔";
+      result.style.visibility="visible";
+      timesecond = 6;
+    }
+  }, 1000);
+}
+const addNewRandom = () => {
+  setInterval(() => {
+    randomVar = Math.floor(Math.random() * 6) + 1;
+    dice.src = `img/load.gif`;
+    dice.style.width = "37.5%";
+    // result.innerHTML = "";
+    document.getElementById("text-user-selected").style.visibility="hidden";
+    Array.from(buttonVal).forEach(element => {
+      element.disabled = false;
+      element.classList.remove("disabled");
+    });
+  }, 10000);
+}
+function musicImageHandler(){
+  if (playMusic){
+    playMusic = false;
+    document.getElementById("image-music").src = `img/music-off.png`;
+  }else{
+    playMusic = true;
+    document.getElementById("image-music").src = `img/music-on.png`;
+  }
+}
+function playSound(state){
+  if(playMusic){
+    if (state == "right"){
+      var audio = new Audio("./assets/audio/success-1-6297.mp3");
+      audio.play();
+    }else if(state == "wrong"){
+      var audio = new Audio("./assets/audio/negative_beeps-6008.mp3");
+      audio.play();
+    }
+  } 
+}
+window.onload = () => {
+  load();
+  DisplayTime();
+  addNewRandom();
+}
